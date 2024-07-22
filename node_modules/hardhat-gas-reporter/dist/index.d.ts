d59391b237bb2fe656a95b7c3fa8865937302ab4
Hawwal Ogungbadero <98647296+Hawwal@users.noreply.github.com>
2024-07-22 01:38:57 +0100

    Delete node_modules directory

--- node_modules/hardhat-gas-reporter/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { EIP1193Provider } from "hardhat/types";
-import "./type-extensions";
-import { GasReporterExecutionContext } from "./types";
-export declare function initializeGasReporterProvider(provider: EIP1193Provider, context: GasReporterExecutionContext): Promise<void>;
-//# sourceMappingURL=index.d.ts.map
--- node_modules/hardhat-gas-reporter/dist/lib/validators/foundry.js
+++
@@ -1,2 +0,0 @@
-/* placeholder */
-//# sourceMappingURL=foundry.js.map
